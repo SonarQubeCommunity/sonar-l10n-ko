c1a8c0f8fa5aeba51c7bf04ffdd0cc8b5fd1fa35
kim.moses@cashslide.co.kr <creatinov.kim@gmail.com>
2015-01-28 06:26:47 +0900

    initial commit

---
+++ src/test/java/org/sonar/plugins/l10n/KoreanPackPluginTest.java
@@ -0,0 +1,37 @@
+/*
+ * L10n :: Korean Pack
+ * Copyright (C) 2015 Creatinov
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.plugins.l10n;
+
+import org.junit.Test;
+import org.sonar.test.i18n.I18nMatchers;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public class KoreanPackPluginTest {
+  @Test
+  public void bundles_should_be_up_to_date() {
+    I18nMatchers.assertBundlesUpToDate();
+  }
+
+  @Test
+  public void should_not_declare_extensions() {
+    assertThat(new KoreanPackPlugin().getExtensions()).isEmpty();
+  }
+}
